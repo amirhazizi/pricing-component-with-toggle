7768daf369ea048b9b618c06439b65f600684b0b
amir <amirazizihosein@gmail.com>
2023-03-13 23:56:40 +0330

    add js

---
+++ js/data.js
@@ -0,0 +1,14 @@
+export default data = [
+  // aunnually
+  {
+    base: 199.99,
+    professional: 249.99,
+    master: 399.99,
+  },
+  // monthly
+  {
+    base: 19.99,
+    professional: 24.99,
+    master: 39.99,
+  },
+]
